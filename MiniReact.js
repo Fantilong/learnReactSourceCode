7c6047621678eacc5a466b01b17069850e5a0e9c
fantl <fantl@shanhs.com>
2022-11-01 18:06:39 +0800

    feat: 懒回调函数，还没完全看懂

--- MiniReact.js
+++ MiniReact.js
@@ -42,7 +42,6 @@ function createDom(fiber) {
   return dom
 }
 
-
 /**
  * 渲染元素到dom上
  * @param {*} element 
@@ -85,6 +84,48 @@ requestIdleCallback(workLoop)
 
 function performUnitOfWork(fiber) {
   // TODO add dom node
+  if(!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if(fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
   // TODO create new fibers
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    }
+
+    if(index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+  
+    prevSibling = newFiber
+    index++
+  }
+
   // TODO return next unit of work
+  if(fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if(nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
