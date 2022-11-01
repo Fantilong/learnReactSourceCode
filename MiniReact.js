39f0e1ee5e821830f16cd39ced38352f20cc7415
fantl <fantl@shanhs.com>
2022-11-01 16:25:43 +0800

    feat: 复写 render 函数

--- MiniReact.js
+++ MiniReact.js
@@ -1,8 +1,45 @@
-class MiniReact {
-  createElement(tag, attr, children) {
+const TEXT_ELEMENT = 'TEXT_ELEMENT'
 
+class MiniReact {
+  constructor(props){}
+  // 创建一个生成 ReactElement 的方法
+  createElement(type, props, ...children) {
+    /* 
+      React 创建 element 的方式
+      React.createElement(
+        'div',
+        {id: 'foo'},
+        React.createElement('a', null, 'bar'),
+        React.createElement('b'),
+      )
+    */
+   return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => (typeof child === 'object' ? child : this.createTextElement(child)))
+    }
+   }
+  }
+  createTextElement(text) {
+    return {
+      type: TEXT_ELEMENT,
+      props: {
+        nodeValue: text,
+        children: []
+      }
+    }
+  }
+  render(element, container) {
+    const dom = element.type === TEXT_ELEMENT ? document.createTextNode('') : document.createElement(element.type)
+    const isProperty = key => key !== 'children'
+    Object.keys(element.props).filter(isProperty).forEach(name => dom[name] = element.props[name])
+
+    element.props.children.forEach(child => this.render(child, dom))
+    container.appendChild(dom)
   }
 }
+
 class MiniReactDOM {
   render(el, container) {
     const node = document.createElement(ele.type)
